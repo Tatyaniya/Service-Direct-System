75a7953669515ab0e438e0dfa8f55a9b0e6449ac
Tatyaniya <tatyaniya@bigmir.net>
2019-05-27 23:25:59 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
 $( function() {
 
-    console.log(1);
-
     // Modal
 
-    $('.call').click( function (e) {
-        e.preventDefault();
-        $('#exampleModal').arcticmodal({
-            overlay: {
-                css: {
-                    backgroundColor: 'rgba(19, 23, 49, .93)',
-                    opacity: 1
-                }
-            }
-        });
+    $('#modal1').click( function () {
+        $('#exampleModal1').arcticmodal();
+    });
+
+    $('#modal2').click( function () {
+        $('#exampleModal2').arcticmodal();
+    });
+
+    $('#modal3').click( function () {
+        $('#exampleModal3').arcticmodal();
+    });
+
+    $('#modal4').click( function () {
+        $('#exampleModal4').arcticmodal();
+    });
+
+    $('#modal5').click( function () {
+        $('#exampleModal5').arcticmodal();
+    });
+
+    $('#modal6').click( function () {
+        $('#exampleModal6').arcticmodal();
     });
 
     // Scroll
 
-    $(".more-info").click( function(e) {
+    $(".menu__link").click( function(e) {
         e.preventDefault();
 		elementClick = $(e.currentTarget).attr("href");
 		destination = $(elementClick).offset().top;
         $("body,html").animate({scrollTop: destination }, 800);
-    });
-
-    // Menu opener hamburger
-
-    $('.hamburger').click( function() {
-        $('.menu-collapse').toggleClass('d-none');
-        $('.menu').toggleClass('menu-opened');
-        $('.hamburger').toggleClass('hamburger-opened');
-        $('.hamburger-s').toggleClass('hamburger-s-opened');
-        $('.hamburger-x').toggleClass('hamburger-x-opened');
     });
 
 });
